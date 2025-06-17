dd42e7089bdafd2bde30b9bcbd8576a7ab15e333
Yaman Abdullah <yamanabdullah370@gmail.com>
2025-06-17 11:47:00 +0300

    Create registerApi for the resturant

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db'); 
+const errorHandlerRegisterMiddleware = require('./middleware/registerErrorHandler');
+const registerRouter = require('./routes/registerRoute');
 
+app.use(express.json());
 connectDB();  
+
+app.use(registerRouter);
+app.use(errorHandlerRegisterMiddleware);
 
 app.listen(3000, '0.0.0.0', () => {
     console.log("Server is running on port 3000");
---
+++ controllers/registerController.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const Token = require('../models/token');
+
+const createTokens = (id) => {
+    const accessToken = jwt.sign( { id }, process.env.secret, {
+        expiresIn: '5m'
+    });
+
+    const refreshToken = jwt.sign( { id }, process.env.secret, {});
+    return {accessToken, refreshToken};
+};
+
+module.exports.register_post = async (req, res, next) => {
+    try {
+        const { username, password, confirmPassword, email} = req.body;
+
+        if (password != confirmPassword) {
+            return res.status(400).json({ message: 'Password and confirm password do not match' });
+        }
+
+        const newUser = await User.create({ username, hashPassword: password, email});
+        await newUser.save();
+
+        const { accessToken, refreshToken} = createTokens(newUser._id);
+        const token = new Token({
+            userId: newUser._id,
+            refreshToken,
+        });
+        token.save();
+
+        res.status(201).json( {message: 'Successful registration', refreshToken, accessToken});
+    }
+    catch(err) {
+        next(err);
+    }
+}
