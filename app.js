78049af0b0cc38e43fe3a6f30050626457dd96b7
Yaman Abdullah <yamanabdullah370@gmail.com>
2025-06-17 11:18:25 +0300

    Create DB for resturant

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db'); 
+
+connectDB();  
 
 app.listen(3000, '0.0.0.0', () => {
     console.log("Server is running on port 3000");
